51853862d78fe252136c7854607f53fec6d18c76
nian0114 <268078545@qq.com>
2013-12-14 23:53:45 +0800

    Fix error

--- out/SC03E/AOSP/obj/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Dec 14 23:31:34 CST 2013"
-#define LINUX_COMPILE_BY "Nian"
-#define LINUX_COMPILE_HOST "Nian-PC"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- out/SC03E/AOSP/obj/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.0.101-Nian-CherryS.AOSP-V0.2-Nian-g791ac14-dirty"
